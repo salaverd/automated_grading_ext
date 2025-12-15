109e8d0a78cbfb84da5ba6881bb520aa75bf579e
salaverd <sim.alaverdyan.02@gmail.com>
2025-12-15 16:23:38 +0400

    correctly working syntax checking and adding custom feedbacks where needded. Auto-Next is not tested, as well as correct pattern

--- content.js
+++ content.js
@@ -2,14 +2,16 @@
 const STUDENT_CONTAINER_SELECTOR = "div.examiner-activity-container-components-wrapper";
 const EXAMINER_COMPONENT_SELECTOR = "div.examiner-component-feedback";
 const CODEPEN_LINK_SELECTOR = "a[href*='codepen.io']";
+const LINK_SELECTOR = "div.examiner-task-text-answer-content";
 const AWARD_BUTTON_SELECTOR = "button.examiner-component-feedback-status-button-award";
 const REJECT_BUTTON_SELECTOR = "button.examiner-component-feedback-status-button-reject";
 // const REJECT_BUTTON_SELECTOR = "button.examiner-component-feedback-status-button-reject.feedback-status-buttons-focus.v-btn.v-btn--outlined.theme--dark.v-size--default.examiner-tab-stop";
 // const NEXT_BUTTON_SELECTOR = "button.examiner-next-and-save-button.v-btn v-btn--text.theme--light.v-size--large";
 const NEXT_BUTTON_SELECTOR = "button.examiner-next-button.v-btn.v-btn--has-bg.theme--light.v-size--large.examiner-tab-stop";
-const REJECT_LIST_SELECTOR = "div.v-item-group.theme--light.v-list-item-group"
+const REJECT_LIST_SELECTOR = "div.v-item-group.theme--light.v-list-item-group";
 // ------- TODO check if this class is correct
-const ACTIVITY_COMPONENT_SELECOR = "div.examiner-activity-container-components-wrapper"; 
+const ACTIVITY_COMPONENT_SELECOR = "div.examiner-activity-container-components-wrapper";
+const FINAL_FEEDBACK_SELECTOR = "div.v-select__slot";
 
 function bySelectorOrText(selectorStr, root = document) {
     const parts = selectorStr.split(',').map(p => p.trim());
@@ -131,18 +133,33 @@ let stopRequested = false;
 let isProcessing = false;
 
 // --------- TODO - check this function ------------
-function getRejectListItems() {
-    const root = document.querySelector(ACTIVITY_COMPONENT_SELECOR);
+// function getListItems(root = document.querySelector(ACTIVITY_COMPONENT_SELECOR)) {
+function getListItems(root) {
+    // const root = document.querySelector(ACTIVITY_COMPONENT_SELECOR);
     if (!root) return [];
 
-    const container = root.querySelector(REJECT_LIST_SELECTOR);
+    // const container = root.querySelector(REJECT_LIST_SELECTOR);
+    const container = root;
+    console.log("container inside getListItems: ", container);
+
     if (!container) return [];
 
     // let arr = Array.from(container.querySelectorAll('.v-list-item, .v-item, .v-list-item__content'))
     //     || Array.from(container.children);
 
-    return Array.from(container.querySelectorAll("input[type='checkbox'], [role='checkbox']"));
+    // return Array.from(container.querySelectorAll("input[type='checkbox'], [role='checkbox']"));
+    return Array.from(container.querySelectorAll("input[type='checkbox'], [role='checkbox'], input[type='radio'], [role='radio']"
+    ));
 
+}
+
+function isCodePenUrl(url) {
+    try {
+        const parsed = new URL(url);
+        return parsed.hostname === "codepen.io";
+    } catch (e) {
+        return false;
+    }
 }
 
 async function processCurrentStudent(autoNext) {
@@ -156,131 +173,211 @@ async function processCurrentStudent(autoNext) {
     while (!stopRequested) {
         try {
             const studentRoot = document.querySelector(STUDENT_CONTAINER_SELECTOR) || document;
-            const examinationRoot = document.querySelector(EXAMINER_COMPONENT_SELECTOR) || document;
+            // const examinationRoot = document.querySelector(EXAMINER_COMPONENT_SELECTOR) || document;
+            const examinationRoot = document.querySelector(ACTIVITY_COMPONENT_SELECOR) || document;
             const rejectEl = bySelectorOrTextAll(REJECT_BUTTON_SELECTOR);
             const awardEl = bySelectorOrTextAll(AWARD_BUTTON_SELECTOR);
-            // console.log(rejectEl);
-            // console.log(awardEl);
 
-            // gather codepen anchors inside studentRoot
-            const anchors = Array.from(studentRoot.querySelectorAll(CODEPEN_LINK_SELECTOR));
-            if (!anchors.length) {
-                console.warn('No CodePen links found in student container. Check STUDENT_CONTAINER_SELECTOR and CODEPEN_LINK_SELECTOR.');
-                break;
-            }
+            // ---- here -----
+            // const groups = Array.from(examinationRoot.querySelectorAll('.v-item-group.theme--light.v-list-item-group'));
+            // const desiredGroup = groups[0];
 
-            // choose up to 3 relevant pens
+            // console.log("groups[0] = ", groups[0]);
+
+            // const items = getListItems(desiredGroup);
+            // console.log(items);
+            // items[1].click();
+            // --------
+
+            // // gather codepen anchors inside studentRoot
+            // const anchors = Array.from(studentRoot.querySelectorAll(CODEPEN_LINK_SELECTOR));
+            // if (!anchors.length) {
+            //     console.warn('No CodePen links found in student container. Check STUDENT_CONTAINER_SELECTOR and CODEPEN_LINK_SELECTOR.');
+            //     break;
+            // }
+
+            // // choose up to 3 relevant pens
             // const pens = anchors.slice(0, 3).map(a => a.href);
-            const pens = ["https://codepen.io/Sima-Alaverdyan-the-animator/pen/bNpmMbq?editors=0010", "https://codepen.io/Armen-Aghayan/pen/PwNaJzM?editors=0011", "https://codepen.io/Sima-Alaverdyan-the-animator/pen/bNpmMbq?editors=0010"];
-            console.log('Grader found CodePen links:', pens);
+            const pens = ["https://codepen.io/Sima-Alaverdyan-the-animator/pen/yyOWRvX?editors=0010", 
+                "https://codepen.io/Armen-Aghayan/pen/PwNaJzM?editors=0011", 
+                "https://codepen.io/Sima-Alaverdyan-the-animator/pen/WbwBaMG?editors=0010"];
+            // console.log('Grader found CodePen links:', pens);
+
+            // const div = studentRoot.querySelector(LINK_SELECTOR);
+            // const divs = bySelectorOrTextAll(studentRoot, LINK_SELECTOR);
+
+            // let pens = [];
+            // const divs = studentRoot.querySelectorAll(LINK_SELECTOR);
+
+            // if (!divs) {
+            //     console.warn("Div not found");
+            //     return;
+            // }
+            // divs.forEach(div => {
+            //     const text = div.textContent.trim();
+            //     pens.push(text);
+            // });
 
             let anyFail = false;
             const details = [];
             // const failedIndexes = [];
+            // i is for tracking each exercise 
             let i = 0;
+            // rejectIndx is for choosing reject reason from the list.
+            // default is 1
+            let rejectIndx = 1;
+            const groups = Array.from(examinationRoot.querySelectorAll('.v-item-group.theme--light.v-list-item-group'));
 
             for (const penUrl of pens) {
                 if (stopRequested) break;
                 console.log('Fetching pen:', penUrl);
-                const html = await fetchPenHTML(penUrl);
-                if (!html) {
+                if (!isCodePenUrl(penUrl)) {
                     anyFail = true;
-                    details.push({ pen: penUrl, reason: 'Failed to fetch pen HTML' });
-                    break;
-                }
-
-                const resp = await bgSend({ cmd: 'fetchPen', url: penUrl });
-                let js;
-                if (resp && resp.html) {
-                    js = extractJs(resp.html);
-                    console.log('Extracted JS:\n', js);
-                } else {
-                    console.warn('No HTML returned from background fetch', resp);
-                }
-
-                const result = checkSyntaxWithAcorn(js);
-                details.push(result.error);
-
-                if (result.valid) {
-                    console.log("Syntax is correct!");
-                    if (awardEl[i]) {
-                        awardEl[i].click();
-                        console.log('Marking AWARD for this student.');
-                    }
-                    else {
-                        console.warn('Award button not found; check AWARD_BUTTON_SELECTOR');
-                    }
-                } else {
-                    anyFail = true;
+                    details.push({ pen: penUrl, reason: 'Not a valid URL' });
+                    rejectIndx = 0;
+                    // continue;
                     if (rejectEl[i]) {
                         rejectEl[i].click();
                         // details.push(result.error);
-                        console.log('Marking REJECT for this student. Details:', details, "at ", result.loc);
+                        console.log('Marking REJECT for this student. Details:', details);
                         // console.log("Syntax error:", result.error, "at", result.loc);
-                        let index = 2;
+                        // let index = 2;
                         await new Promise(r => setTimeout(r, 1000));
 
-                        // const checkbox = bySelectorOrTextAll(REJECT_LIST_SELECTOR);
-                        // const container = document.querySelector(REJECT_LIST_SELECTOR, ACTIVITY_COMPONENT_SELECOR);
+                        // const groups = Array.from(examinationRoot.querySelectorAll('.v-item-group.theme--light.v-list-item-group'));
+                        const desiredGroup = groups[i];
 
-                        const items = getRejectListItems();
-                        console.log("items:", items);
-                        items[2].click();
-                        // const container = bySelectorOrTextAll(REJECT_LIST_SELECTOR, ACTIVITY_COMPONENT_SELECOR);
-                        // if (!container) return console.warn("Reject list not found");
-                        // console.log("CONTAINER: ", container);
-
-                        // // Get all checkbox inputs inside it
-                        // const checkboxes = container.querySelectorAll("input[type='checkbox'][role='checkbox']");
-                        // if (!checkboxes.length) return console.warn("No checkboxes found");
-
-                        // const cb = checkboxes[index];
-                        // if (!cb) return console.warn("Index out of range:", index);
-
-                        // // Click is safest for Vuetify
-                        // cb.click();
-
-                        // // Ensure state is updated (sometimes needed)
-                        // cb.checked = true;
-                        // cb.setAttribute("aria-checked", "true");
-                        // cb.dispatchEvent(new Event("change", { bubbles: true }));
-
-                        // console.log("Checked checkbox at index", index);
+                        const items = getListItems(desiredGroup);
+                        console.log(items);
+                        if (!items[rejectIndx].checked) {
+                            items[rejectIndx].click();
+                        }
                     }
-                    else {
-                        console.warn('Reject button not found');
+                }
+                else {
+                    const html = await fetchPenHTML(penUrl);
+                    if (!html) {
+                        anyFail = true;
+                        details.push({ pen: penUrl, reason: 'Failed to fetch pen HTML' });
+                        break;
                     }
-                    // failedIndexes.push(i);
+                    const resp = await bgSend({ cmd: 'fetchPen', url: penUrl });
+                    let js;
+                    if (resp && resp.html) {
+                        js = extractJs(resp.html);
+                        // console.log('Extracted JS:\n', js);
+                    } else {
+                        console.warn('No HTML returned from background fetch', resp);
+                    }
+
+                    const result = checkSyntaxWithAcorn(js);
+                    details.push(result.error);
+
+                    if (result.valid) {
+                        // const groups = Array.from(examinationRoot.querySelectorAll('.v-item-group.theme--light.v-list-item-group'));
+                        // const desiredGroup = groups[i];
+                        // const items = getListItems(desiredGroup);
+                        // console.log("VALID LIST: ", items);
+
+                        console.log("Syntax is correct!");
+                        if (awardEl[i]) {
+                            awardEl[i].click();
+                            await new Promise(r => setTimeout(r, 500));
+
+                            const desiredGroup = groups[i];
+                            const items = getListItems(desiredGroup);
+                            // console.log(items);
+                            if (items.length == 0) {
+                                const value = "Ապրես";
+                                // const slots = document.querySelectorAll(".v-text-field__slot");
+                                const slots = bySelectorOrTextAll("div.v-text-field__slot")
+                                const input = slots[i].querySelector("input");
+
+                                input.value = value;
+
+                                input.dispatchEvent(new InputEvent("input", {
+                                    bubbles: true,
+                                    composed: true,
+                                    data: value,
+                                    inputType: "insertText"
+                                }));
+                            }
+                        }
+                        else {
+                            console.warn('Award button not found; check AWARD_BUTTON_SELECTOR');
+                        }
+                    } else {
+                        anyFail = true;
+                        if (rejectEl[i]) {
+                            rejectEl[i].click();
+                            // details.push(result.error);
+                            console.log('Marking REJECT for this student. Details:', details, "at ", result.loc);
+                            // console.log("Syntax error:", result.error, "at", result.loc);
+                            // let index = 2;
+                            await new Promise(r => setTimeout(r, 1000));
+
+                            // const groups = Array.from(examinationRoot.querySelectorAll('.v-item-group.theme--light.v-list-item-group'));
+                            const desiredGroup = groups[i];
+
+                            // console.log(`groups[${i}] = `, i, groups[i]);
+
+                            const items = getListItems(desiredGroup);
+                            // console.log(items);
+                            if (!items[rejectIndx].checked) {
+                                items[rejectIndx].click();
+                            }
+                        }
+                        else {
+                            console.warn('Reject button not found');
+                        }
+                        // failedIndexes.push(i);
+                    }
                 }
                 i++;
             }
+            const feedback = studentRoot.querySelector(FINAL_FEEDBACK_SELECTOR);
+            // const feedback = studentRoot.querySelectorAll(".examiner-tab-stop");
+            const input = feedback?.querySelector("input");
+            const value = anyFail
+                ? "Վերանայիր առաջադրանքը"
+                : "Ապրես :)";
+
+            feedback.dispatchEvent(new MouseEvent("mousedown", {
+                bubbles: true,
+                cancelable: true,
+                view: window
+            }));
+
+            feedback.dispatchEvent(new MouseEvent("mouseup", {
+                bubbles: true,
+                cancelable: true,
+                view: window
+            }));
+
+            feedback.dispatchEvent(new MouseEvent("click", {
+                bubbles: true,
+                cancelable: true,
+                view: window
+            }));
+
+            input.value = value;
+
+            input.dispatchEvent(new InputEvent("input", {
+                bubbles: true,
+                composed: true,
+                data: value,
+                inputType: "insertText"
+            }));
+
+            // document.body.dispatchEvent(new MouseEvent("mousedown", { bubbles: true }));
+            // document.body.dispatchEvent(new MouseEvent("mouseup", { bubbles: true }));
+            // document.body.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+
             if (anyFail) {
                 // Marking the whole exercise as failed.
                 rejectEl[3].click();
-            }
-            // if (anyFail) {
-            //     // const rejectEl = bySelectorOrTextAll(REJECT_BUTTON_SELECTOR);
-            //     console.log('Marking REJECT for this student. Details:', details);
-            //     if (rejectEl) {     // should be fixed
-            //         // rejectEl.click();
-            //         rejectEl.forEach(el => el.click());
-            //         for (let j = 0; j < failedIndexes.length; j++) {
-            //             rejectEl[failedIndexes[j]].click();
-            //         }
-            //     } else {
-            //         console.warn('Reject button not found; check REJECT_BUTTON_SELECTOR');
-            //     }
-            // } else {
-            //     // const awardEl = bySelectorOrTextAll(AWARD_BUTTON_SELECTOR);
-            //     console.log('Marking AWARD for this student. Details:', details);
-            //     if (awardEl) {      // should be fixed
-            //         // awardEl.click();
-            //         awardEl.forEach(el => el.click());
 
-            //     } else {
-            //         console.warn('Award button not found; check AWARD_BUTTON_SELECTOR');
-            //     }
-            // }
+            }
 
             if (!autoNext) {
                 console.log('Auto Next disabled — stopping after current student.');
